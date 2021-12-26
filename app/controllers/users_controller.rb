d1d3ba5b1e6863692cf8ab1611a4ebba63680de9
Blake Eriks <eblake98@vt.edu>
2021-12-25 16:10:57 -0800

    Make user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,35 @@
 class UsersController < ApplicationController
 
-  # POST /users
-  def create
-    @user = User.new(user_params)
+  before_action :authorized, only: [:auto_login]
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+  # REGISTER
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: {error: "Invalid username or password"}
     end
   end
 
-  private
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:username, :password)
+  # LOGGING IN
+  def login
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
     end
+  end
+
+  def auto_login
+    render json: @user
+  end
+  
+  private
+  def user_params
+    params.permit(:username, :password, :age)
+  end
 end
