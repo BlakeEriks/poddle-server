1820f013ea10386e8aa1a8fedb929d03d9930732
Blake Eriks <eblake98@vt.edu>
2021-12-25 16:10:46 -0800

    Clarify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'podcasts/search', to: 'podcasts#search'
-  resources :podcasts
+  get 'podcasts/my_list', to: 'podcasts#myList'
+  get 'podcasts/top', to: 'podcasts#indexTop'
+  delete 'podcasts/my_list/:id', to: 'podcasts#remove'
+  resources :podcasts, only: [:index, :show, :create, :destroy]
   resource :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
