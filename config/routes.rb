b2d0208e58209523fdbac2b582bce9743f4d5a99
Blake Eriks <eblake98@vt.edu>
2022-01-03 10:29:11 -0800

    Refactor for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+
   get 'podcasts/search', to: 'podcasts#search'
   get 'podcasts/my_list', to: 'podcasts#myList'
   get 'podcasts/top', to: 'podcasts#indexTop'
   delete 'podcasts/my_list/:id', to: 'podcasts#remove'
-  resources :podcasts, only: [:index, :show, :create, :destroy]
+  post 'podcasts/my_list', to: 'podcasts#add'
+  resources :podcasts, only: [:index, :show, :destroy]
+
   resource :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
+
+  get 'genres/my_list', to: 'genres#myGenres'
+  put 'genres/my_list', to: 'genres#update'
+  resources :genres, only: [:index]
+
 end
