a882e00a38ed77d9707b3ed0c048be24bb57763b
Blake Eriks <eblake98@vt.edu>
2022-01-03 21:46:18 -0800

    Remove destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'podcasts/top', to: 'podcasts#indexTop'
   delete 'podcasts/my_list/:id', to: 'podcasts#remove'
   post 'podcasts/my_list', to: 'podcasts#add'
-  resources :podcasts, only: [:index, :show, :destroy]
+  resources :podcasts, only: [:index, :show]
 
   resource :users, only: [:create]
   post "/login", to: "users#login"
